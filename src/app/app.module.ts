7eb3f2df0b60452f69231f3085ce2195d8cc53b0
fenas <fenas.fnz@gmail.com>
2023-07-06 10:11:13 +0530

    added initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    BooksListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/books-list/books-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './books-list.component.html',
+  styleUrls: ['./books-list.component.css'],
+})
+export class BooksListComponent {
+  Books: any = [];
+  constructor(private crudService: CrudService) {
+    this.crudService.GetBooks().subscribe((data: any) => {
+      this.Books = data.data;
+    });
+  }
+  ngOnInit() {}
+  delete(id: any, i: any) {
+    if (window.confirm('Do you want to go ahead?')) {
+      this.crudService.deleteBook(id).subscribe((data: any) => {
+        this.Books.splice(i, 1);
+      });
+    }
+  }
+}
